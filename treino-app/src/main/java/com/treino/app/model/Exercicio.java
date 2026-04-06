e5d251de23ab415a3a6cb76e7ca247762ae26a4a
Kaio <kaio.bezerra1809@gmail.com>
2026-04-06 18:08:49 -0300

    Add one-to-many relationship for ExecucaoExercicio

--- treino-app/src/main/java/com/treino/app/model/Exercicio.java
+++ treino-app/src/main/java/com/treino/app/model/Exercicio.java
@@ -1,6 +1,9 @@
 package com.treino.app.model;
 
 import jakarta.persistence.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 @Entity
 public class Exercicio {
@@ -10,6 +13,9 @@ public class Exercicio {
     private Long id;
 
     private String nome;
+
+    @OneToMany(mappedBy = "exercicio")
+    private List<ExecucaoExercicio> execucoes = new ArrayList<>();
 
     public Exercicio() {}
 
@@ -27,5 +33,9 @@ public class Exercicio {
 
     public void setNome(String nome) {
         this.nome = nome;
+    }
+
+    public List<ExecucaoExercicio> getExecucoes() {
+        return Collections.unmodifiableList(execucoes);
     }
 }
